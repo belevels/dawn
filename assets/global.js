e16e37351b37465159baf38874bae7f0bb64d5b0
belevels <93495071+belevels@users.noreply.github.com>
2023-01-08 23:12:59 +0100

    .

--- assets/global.js
+++ assets/global.js
@@ -792,7 +792,7 @@ class VariantSelects extends HTMLElement {
   updateMasterId() {
     this.currentVariant = this.getVariantData().find((variant) => {
       return !variant.options.map((option, index) => {
-        document.getElementById("currently-selected-variation").innerHTML = this.options[index];
+        document.getElementById("currently-selected-variation").innerHTML = metaData[id];
         return this.options[index] === option;
       }).includes(false);
     });
